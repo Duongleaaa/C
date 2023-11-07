f58fefd2b3b8315f4489f807032c958294acb932
Duongleaaa <147384085+Duongleaaa@users.noreply.github.com>
2023-11-08 04:43:12 +0700

    Add files via upload

---
+++ BTSS2/BTVN1ss2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float chv= 5;
+	float cdhcn=6;
+	float crhcn=4;
+	float bkdt=7;
+	const float PI=3.14;
+	float cvhv= chv*4;
+	float dthv= chv*chv;
+	float cvhcn= (cdhcn+crhcn)*2;
+	float dthcn=cdhcn*crhcn;
+	float cvhTron= (bkdt*2)*PI;
+	float dthTron= (bkdt*bkdt)*PI;
+}
